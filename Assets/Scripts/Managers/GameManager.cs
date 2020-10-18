abd2a6051f194d737978aaaedaa3eb04a041b9dd
Rhys Stever <rhys@stever.com>
2020-10-17 19:50:32 -0700

    Jump implemented

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -29,7 +29,11 @@ public class GameManager : MonoBehaviour
 			if(Vector3.Distance(player.transform.position, npc.Value.transform.position) < 10.0f)
 			{
 				adjacentNPC = npc.Value;
-				npc.Value.transform.LookAt(player.transform);
+				Vector3 lookAtPos = new Vector3(
+					player.transform.position.x,
+					player.GetComponent<BoxCollider>().size.y / 2,
+					player.transform.position.z);
+				npc.Value.transform.LookAt(lookAtPos);
 			}
 		}
 	}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,27 +6,57 @@ public class Movement : MonoBehaviour
 {
 	public float moveSpeed;
 	public float turnSpeed;
+	public float jumpAmount;
+	public float gravity;
+	Vector3 acceleration;
 
 	// Start is called before the first frame update
 	void Start()
     {
 		moveSpeed = 10.0f;
-		turnSpeed = 50.0f;
+		turnSpeed = 100.0f;
+		jumpAmount = 15.0f;
+		acceleration = new Vector3();
 	}
 
     // Update is called once per frame
     void FixedUpdate()
 	{
 		BasicMovement();
+		VerticalMovement();
 	}
 
 	void BasicMovement()
 	{
 		// Foward / Backward movement
 		float movement = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-		float turning = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
-
 		transform.Translate(0, 0, movement);
+
+		// Turning 
+		float turning = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
 		transform.Rotate(0, turning, 0);
+	}
+
+	void VerticalMovement()
+	{
+		if(gameObject.transform.position.y > 0.85f)
+		{
+			acceleration.y -= gravity * Time.deltaTime;
+		}
+		else if(gameObject.transform.position.y <= 0.85f)
+		{
+			gameObject.transform.position = new Vector3(
+				gameObject.transform.position.x,
+				0.85f,
+				gameObject.transform.position.z);
+
+			acceleration = new Vector3();
+
+			float jumpAccel = Input.GetAxis("Jump") * Time.deltaTime * jumpAmount;
+			acceleration.y += jumpAccel;
+			//transform.Translate(0, jump, 0);
+		}
+
+		transform.Translate(acceleration);
 	}
 }
